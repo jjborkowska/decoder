01447ab6991277323d2a2c6ea9631c9744f4f5b5
jjborkowska <jjb.jjborkowska@gmail.com>
2025-10-28 11:45:36 +0100

    Initial Commit

---
+++ gaderypoluki.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+//Entering the string
+  print('Enter your string: ');
+  String ?letter = stdin.readLineSync();
+
+  //test
+  print('Your string: $letter');
+
+
+  //switch ()
+}
